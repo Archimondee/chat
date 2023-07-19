0776b498f2df708fbe210f0193fc1430b30a47b6
Gilang <gilang@62teknologi.com>
2023-07-19 17:06:14 +0700

    First init

---
+++ config/app.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+// Config stores all configuration of the application.
+// The values are read by viper from a config file or environment variable.
+type Config struct {
+	Environment       string `mapstructure:"ENVIRONMENT"`
+	DBDriver          string `mapstructure:"DB_DRIVER"`
+	DBSource          string `mapstructure:"DB_SOURCE"`
+	HTTPServerAddress string `mapstructure:"HTTP_SERVER_ADDRESS"`
+}
+
+// LoadConfig reads configuration from file or environment variables.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
